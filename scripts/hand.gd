5afa99a87c3fdfdb6e90053c7a9a7941f3591c80
Waffle6056 <114373955+Waffle6056@users.noreply.github.com>
2022-11-22 22:15:19 -0600

    pick up objects
    rigidbodies on layer 6 can be picked up by pressing e

---
+++ scripts/hand.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+var item
+var currentlytouched = false
+var holding = false
+export(NodePath) var fasd
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	rotation_degrees = 0
+	look_at(get_global_mouse_position())
+	rotation_degrees += 90
+	if !holding:
+		if Input.is_action_just_pressed("click") and currentlytouched:
+			holding = true
+	else:
+		item.position = get_children()[0].global_position
+		if Input.is_action_just_pressed("click"):
+			holding = false
+
+
+
+func _on_Hand_body_entered(body):
+	if !holding:
+		item = body
+		currentlytouched = true
+	print(body)
+
+func _on_Hand_body_exited(body):
+	if !holding:
+		currentlytouched = false
